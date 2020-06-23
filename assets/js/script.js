c349c110dc977d91da8c69830a1e17c2574ee998
JoeL <jlawton1203@gmail.com>
2020-06-23 11:29:02 -0500

    added drag and drop to allow list sorting and moving tasks between lists. added delete functionality by dragging tasks to the trash zone

--- assets/js/script.js
+++ assets/js/script.js
@@ -184,6 +184,78 @@ $("#remove-tasks").on("click", function() {
   saveTasks();
 });
 
+//enable tasks to be sortable within their own list and dragged between lists
+$(".card .list-group").sortable({
+  connectWith: $(".card .list-group"),
+  scroll: false,
+  tolerance: "pointer",
+  helper: "clone",
+  activate: function(event) {
+    //console.log("activate", this);
+  },
+  deactivate: function(event) {
+    //console.log("deactivate", this);
+  },
+  over: function(event) {
+    //console.log("over", event.target);
+  },
+  out: function(event) {
+    //console.log("out", event.target);
+  },
+  update: function(event) {
+
+    //temporary array to store task data in to prepare for saving
+    var tempArr = [];
+
+    //loop over current set of children in sortable list
+    //console.log($(this).children());
+    $(this).children().each(function() {
+      var text = $(this)
+        .find("p")
+        .text()
+        .trim()
+      
+      var date = $(this)
+        .find("span")
+        .text()
+        .trim()
+
+      //add the task data to tempArray
+      tempArr.push({
+        text: text,
+        date: date
+      });
+
+      //console.log(tempArr);
+
+    });
+
+    // trim down list's ID to match object property
+    var arrName = $(this)
+    .attr("id")
+    .replace("list-", "");
+
+    // update array on tasks object and save
+    tasks[arrName] = tempArr;
+    saveTasks();
+  }
+});
+
+$("#trash").droppable({
+  accept: ".card .list-group-item",
+  tolerance: "touch",
+  drop: function (event, ui) {
+    console.log("drop");
+    ui.draggable.remove();
+  },
+  over: function(event, ui) {
+    //console.log("over");
+  },
+  out: function(event, ui) {
+    //console.log("out");
+  }
+});
+
 // load tasks for the first time
 loadTasks();
 
