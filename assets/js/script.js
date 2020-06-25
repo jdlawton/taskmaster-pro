0e8ebc3d19cca0b3875531030e671fdc005a0d11
JoeL <jlawton1203@gmail.com>
2020-06-24 21:31:08 -0500

    added drag and drop highlighting and trash hidden until dragging

--- assets/js/script.js
+++ assets/js/script.js
@@ -207,15 +207,21 @@ $(".card .list-group").sortable({
   helper: "clone",
   activate: function(event) {
     //console.log("activate", this);
+    $(this).addClass("dropover");
+    $(".bottom-trash").addClass("bottom-trash-drag");
   },
   deactivate: function(event) {
     //console.log("deactivate", this);
+    $(this).removeClass("dropover");
+    $(".bottom-trash").removeClass("bottom-trash-drag");
   },
   over: function(event) {
     //console.log("over", event.target);
+    $(event.target).addClass("dropover-active");
   },
   out: function(event) {
     //console.log("out", event.target);
+    $(event.target).removeClass("dropover-active");
   },
   update: function(event) {
 
@@ -265,9 +271,11 @@ $("#trash").droppable({
   },
   over: function(event, ui) {
     //console.log("over");
+    $(".bottom-trash").addClass("bottom-trash-active");
   },
   out: function(event, ui) {
     //console.log("out");
+    $(".bottom-trash").removeClass("bottom-trash-active");
   }
 });
 
